a365453ef171223920725bbec71023960c180a56
Robert Sörensen <robert.sorensen44@gmail.com>
2021-02-01 12:57:47 +0100

    comments

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
+/*
+ * Jag debuggade genom att sätta breakpoints inne i sources verktyget inne i chrome. Där kollade jag så alla variabler fick rätt värde.
+ * När något inte gick rätt till så satte jag in en alert på det ställe som inte funkade. 
+ * Så tex. när ett event skulle ske men det inte gick på rätt sätt så hade jag alert där för att se hur långt koden gick. 
+ */ 
 window.onload = function () {
   document.getElementById("nr").oninput = function () {
     Remove()
 
-    var posts = parseInt(this.value)
+    var posts = parseInt(this.value) // här får variabeln posts värdet av inputen.
 
-    for (var i = 0; i < posts; i++) {
+    for (var i = 0; i < posts; i++) { //går sedan igenom en loop för att skapa så många av varje element.
+
       var place = document.querySelector("input")
       var parent = document.querySelector("section")
       var navi = document.querySelector("div")
+      //här väljer vi vad vi skapar i section.
 
       var arti = document.createElement("article")
 
@@ -15,6 +22,7 @@ window.onload = function () {
       var txt = document.createElement("p")
       var link = document.createElement("a")
 
+      // här väljer vi innehållet i de olika elementen och id för article samt href för länkarna.
       place.placeholder = posts
       arti.id = i + 1
       heads.innerHTML = "Post # " + (i + 1)
@@ -23,7 +31,7 @@ window.onload = function () {
       link.innerHTML = "Post # " + (i + 1)
       link.href = "#" + (i + 1)
 
-        makeEditable(heads)
+        makeEditable(heads) // här skickar vi txt o heads till en function längre ner. så vi kan redigera texten.
         makeEditable(txt)
 
       arti.appendChild(heads)
@@ -34,9 +42,9 @@ window.onload = function () {
   }
 }
 
-function Remove() {
+function Remove() { // här tar vu bort alla article och länkar.
   var blogs = document.querySelectorAll("article")
-  var links = document.querySelectorAll("a")
+  var links = document.querySelectorAll("a") 
   for (var i = 0; i < blogs.length; i++) {
     blogs[i].remove()
     links[i].remove()
@@ -44,7 +52,7 @@ function Remove() {
 }
 
 function makeEditable(elem) {
-  elem.onclick = function () {
+  elem.onclick = function () { // när användaren trycker på dessa elem så kan man redigera texten samt får en border runt.
     elem.contentEditable = true
     elem.focus()
   }
--- js/script.js
+++ js/script.js
@@ -1,34 +1,35 @@
-const newblog = document.querySelector(".new")
+const newblog = document.querySelector(".new") // när vi trycker på new post så sker detta event.
 newblog.onclick = function newpost() {
-    var divs = document.getElementById("nr").placeholder
-    parseInt(divs)
-    divs++
-    var divvs = divs
-    document.getElementById("nr").placeholder = divvs
+  // vi tar värdet från input och höjer det med 1.
+  //sedan gör vi samma som i main.js men vi byter även ut input placeholder till det nya värdet.
+  var divs = document.getElementById("nr").placeholder;
+  parseInt(divs);
+  divs++;
+  var divvs = divs;
+  document.getElementById("nr").placeholder = divvs;
 
-    var place = document.querySelector("input")
-    var parent = document.querySelector("section")
-    var navi = document.querySelector("div")
+  var place = document.querySelector("input");
+  var parent = document.querySelector("section");
+  var navi = document.querySelector("div");
 
-    var arti = document.createElement("article")
+  var arti = document.createElement("article");
 
-    var heads = document.createElement("h3")
-    var txt = document.createElement("p")
-    var link = document.createElement("a")
-    
-    place.placeholder = divvs
-    place.value = divvs
-    arti.id = divvs
-    heads.innerHTML = "Post # " + divvs
-    txt.innerHTML =
-      "Click me"
-    link.innerHTML = "Post # " + divvs
-    link.href = "#" + divvs
-    makeEditable(heads)
-    makeEditable(txt)
+  var heads = document.createElement("h3");
+  var txt = document.createElement("p");
+  var link = document.createElement("a");
 
-    arti.appendChild(heads)
-    arti.appendChild(txt)
-    navi.appendChild(link)
-    parent.appendChild(arti)
+  place.placeholder = divvs;
+  place.value = divvs;
+  arti.id = divvs;
+  heads.innerHTML = "Post # " + divvs;
+  txt.innerHTML = "Click me";
+  link.innerHTML = "Post # " + divvs;
+  link.href = "#" + divvs;
+  makeEditable(heads);
+  makeEditable(txt);
+
+  arti.appendChild(heads);
+  arti.appendChild(txt);
+  navi.appendChild(link);
+  parent.appendChild(arti);
 }
