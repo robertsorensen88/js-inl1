e120f17865ec9fcfacdaa7679e898ca32d8f02eb
Robert Sörensen <robert.sorensen44@gmail.com>
2021-02-01 12:28:28 +0100

    klar med allt TODO comments

--- js/main.js
+++ js/main.js
@@ -1,56 +1,54 @@
 window.onload = function () {
   document.getElementById("nr").oninput = function () {
-    Remove();
+    Remove()
 
-    var posts = parseInt(this.value);
+    var posts = parseInt(this.value)
 
     for (var i = 0; i < posts; i++) {
-      var place = document.querySelector("input");
-      var parent = document.querySelector("section");
-      var navi = document.querySelector("div");
+      var place = document.querySelector("input")
+      var parent = document.querySelector("section")
+      var navi = document.querySelector("div")
 
-      var arti = document.createElement("article");
+      var arti = document.createElement("article")
 
-      var heads = document.createElement("h3");
-      var txt = document.createElement("p");
-      var link = document.createElement("a");
+      var heads = document.createElement("h3")
+      var txt = document.createElement("p")
+      var link = document.createElement("a")
 
-      place.placeholder = posts;
-      arti.id = i + 1;
-      heads.innerHTML = "Post # " + (i + 1);
+      place.placeholder = posts
+      arti.id = i + 1
+      heads.innerHTML = "Post # " + (i + 1)
       txt.innerHTML =
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis harum aliquid eos ea! A atque inventore, beatae recusandae commodi placeat tempora, eligendi consectetur laboriosam delectus illo, quia exercitationem. Omnis, esse!";
-      link.innerHTML = "Post # " + (i + 1);
-      link.href = "#" + (i + 1);
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis harum aliquid eos ea! A atque inventore, beatae recusandae commodi placeat tempora, eligendi consectetur laboriosam delectus illo, quia exercitationem. Omnis, esse!"
+      link.innerHTML = "Post # " + (i + 1)
+      link.href = "#" + (i + 1)
 
-        makeEditable(heads);
-        makeEditable(txt);
+        makeEditable(heads)
+        makeEditable(txt)
 
-   
-
-      arti.appendChild(heads);
-      arti.appendChild(txt);
-      navi.appendChild(link);
-      parent.appendChild(arti);
+      arti.appendChild(heads)
+      arti.appendChild(txt)
+      navi.appendChild(link)
+      parent.appendChild(arti)
     }
-  };
-};
+  }
+}
 
 function Remove() {
-  var blogs = document.querySelectorAll("article");
-  var links = document.querySelectorAll("a");
+  var blogs = document.querySelectorAll("article")
+  var links = document.querySelectorAll("a")
   for (var i = 0; i < blogs.length; i++) {
-    blogs[i].remove();
-    links[i].remove();
+    blogs[i].remove()
+    links[i].remove()
   }
 }
 
 function makeEditable(elem) {
   elem.onclick = function () {
-    elem.contentEditable = true;
-    elem.focus();
-  };
+    elem.contentEditable = true
+    elem.focus()
+  }
   elem.onblur = function () {
-    elem.contentEditable = false;
-  };
+    elem.contentEditable = false
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
-const newblog = document.querySelector(".new");
+const newblog = document.querySelector(".new")
 newblog.onclick = function newpost() {
-    var divs = document.getElementById("nr").placeholder;
-    parseInt(divs);
-    divs++;
-    var divvs = divs;
-    document.getElementById("nr").placeholder = divvs;
-    var place = document.querySelector("input");
-    var parent = document.querySelector("section");
-    var navi = document.querySelector("div");
+    var divs = document.getElementById("nr").placeholder
+    parseInt(divs)
+    divs++
+    var divvs = divs
+    document.getElementById("nr").placeholder = divvs
 
-    var arti = document.createElement("article");
+    var place = document.querySelector("input")
+    var parent = document.querySelector("section")
+    var navi = document.querySelector("div")
 
-    var heads = document.createElement("h3");
-    var txt = document.createElement("p");
-    var link = document.createElement("a");
+    var arti = document.createElement("article")
+
+    var heads = document.createElement("h3")
+    var txt = document.createElement("p")
+    var link = document.createElement("a")
     
-    place.placeholder = divvs;
-    place.value = divvs;
-    arti.id = divvs;
-    heads.innerHTML = "Post # " + divvs;
+    place.placeholder = divvs
+    place.value = divvs
+    arti.id = divvs
+    heads.innerHTML = "Post # " + divvs
     txt.innerHTML =
-      "Click me";
-    link.innerHTML = "Post # " + divvs;
-    link.href = "#" + divvs;
-    makeEditable(heads);
-    makeEditable(txt);
+      "Click me"
+    link.innerHTML = "Post # " + divvs
+    link.href = "#" + divvs
+    makeEditable(heads)
+    makeEditable(txt)
 
-    arti.appendChild(heads);
-    arti.appendChild(txt);
-    navi.appendChild(link);
-    parent.appendChild(arti);
-};
+    arti.appendChild(heads)
+    arti.appendChild(txt)
+    navi.appendChild(link)
+    parent.appendChild(arti)
+}
